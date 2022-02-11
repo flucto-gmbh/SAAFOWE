25e133e95bcbcbf6f29fc923bf0530bfe394a47a
Aljoscha Sander <aljoscha@flucto.tech>
2022-02-11 20:13:04 +0100

    fixed a broken import (thanks, code)

--- scripts/report_24h.py
+++ scripts/report_24h.py
@@ -1,7 +1,6 @@
 import argparse
 from datetime import datetime, timezone, timedelta
 from glob import glob
-from click import DateTime
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import numpy as np
