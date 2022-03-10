8d1ab996836d12dad6e0b259133a40aed039f765
Aljoscha Sander <aljoscha.sander@mailbox.org>
2022-03-10 19:20:46 +0100

    update

--- scripts/aggregate_csv.py
+++ scripts/aggregate_csv.py
@@ -13,13 +13,10 @@ from SAAFOWE.io.timefiles import find_time_files, calc_time_intervals
 
 def parse_cmdline() -> dict:
     cmd_parser = argparse.ArgumentParser()
-    cmd_parser.add_argument('--begin', type=str)
-    cmd_parser.add_argument('--end', type=str)
-    cmd_parser.add_argument('--dir', default='.', type=str)
-    cmd_parser.add_argument('--file-pattern', default='*.csv', type=str)
     cmd_parser.add_argument('--verbose', action="store_true")
-    cmd_parser.add_argument('--print0', action='store_true')
-    cmd_parser.add_argument('--print', action='store_true')
+    # parser.add_argument('-i', '--input-file', type=argparse.FileType('r'), default=(None if sys.stdin.isatty() else sys.stdin))
+    # add interval
+    # add stdin 
 
     return cmd_parser.parse_args().__dict__
 
@@ -30,37 +27,13 @@ def main():
     if config['verbose']:
         print(f'config: {config}')
         print(f'sys.path: {sys.path}')
-    try:
-        # first generate a naive time object
-        begin = datetime.fromisoformat(config['begin'])
-        # and now add the timezone
-        begin = begin.replace(tzinfo=timezone.utc)
-    except Exception as e:
-        print(f'failed to parse begin time stamp: {config["begin"]}: {e}')
-        sys.exit()
-    if not config['end']:
-        if config['verbose']: print(f'setting end to NOW')
-        end = datetime.fromtimestamp(time.time(), timezone.utc)
-    else:
-        try:
-            end = datetime.fromisoformat(config['end'])
-            end = end.replace(tzinfo=timezone.utc)
-        except Exception as e:
-            print(f'failed to parse end time stamp: {config["end"]}: {e}')
-            sys.exit()
-    data_dir = path.abspath(config['dir'])
-    if config['verbose']:
-        print(f'looking for files in {data_dir} from {begin} to {end}')
 
-    data_files = find_time_files(file_dir=data_dir, file_pattern=config['file_pattern'], begin=begin, end=end)
-    time_intervals = calc_time_intervals(begin=begin, end=end, interval='hour')
-
-    # print(data_files)
-    if config['print0']: 
-        print(' '.join(data_files))
-    elif config['print']:
-        for d_file in data_files:
-            print(d_file)
+    # 1. get files either from user or from stdin
+    # 2. create dict with files, where the key is the timestamp
+    # 3. get beginning and end from timestamps
+    # 4. calculate the intervals
+    # 5. iterate over files and sort them into a new defaultdict corresponding to each interval
+    # 6. iterate over the intervals, read in files, sanitize files and write them out
 
 if __name__ == "__main__":
     main()
